59d80873e65045799351c5f1a41a42bcfac2ee3c
Lukas Spies <Lukas@photoqt.org>
2015-12-03 15:44:54 -0330

    [CMake] List c++ and qml files in seperate files -> cleaner setup

---
+++ CMake/ListFilesCPlusPlus.cmake
@@ -0,0 +1,58 @@
+#####################
+#### C++ SOURCES ####
+#####################
+
+SET(d "cplusplus")
+SET(photoqt_SOURCES ${photoqt_SOURCES} ${d}/main.cpp ${d}/mainwindow.cpp ${d}/variables.h ${d}/logger.h)
+
+SET(d "cplusplus/handlefiles")
+SET(photoqt_SOURCES ${photoqt_SOURCES} ${d}/loaddir.cpp)
+
+SET(d "cplusplus/imageprovider")
+SET(photoqt_SOURCES ${photoqt_SOURCES} ${d}/imageproviderfull.cpp ${d}/imageproviderthumbnail.cpp ${d}/imageprovidericon.h)
+
+SET(d "cplusplus/shortcuts")
+SET(photoqt_SOURCES ${photoqt_SOURCES} ${d}/shortcuts.h)
+
+SET(d "cplusplus/scripts")
+SET(photoqt_SOURCES ${photoqt_SOURCES} ${d}/getanddostuff.h ${d}/getmetadata.cpp ${d}/runprocess.h ${d}/thumbnailsmanagement.cpp)
+SET(photoqt_SOURCES ${photoqt_SOURCES} ${d}/gmimagemagick.h)
+SET(d "cplusplus/scripts/getanddostuff")
+SET(photoqt_SOURCES ${photoqt_SOURCES} ${d}/context.cpp ${d}/external.cpp ${d}/manipulation.cpp ${d}/file.cpp ${d}/other.cpp)
+SET(photoqt_SOURCES ${photoqt_SOURCES} ${d}/shortcuts.cpp ${d}/wallpaper.cpp ${d}/openfile.cpp)
+
+SET(d "cplusplus/settings")
+SET(photoqt_SOURCES ${photoqt_SOURCES} ${d}/settings.h ${d}/settingssession.h ${d}/fileformats.h)
+
+SET(d "cplusplus/singleinstance")
+SET(photoqt_SOURCES ${photoqt_SOURCES} ${d}/singleinstance.cpp)
+
+
+####################
+#### C++ HEADER ####
+####################
+
+SET(d "cplusplus")
+SET(photoqt_HEADERS ${photoqt_HEADERS} ${d}/mainwindow.h ${d}/variables.h ${d}/logger.h)
+
+SET(d "cplusplus/handlefiles")
+SET(photoqt_HEADERS ${photoqt_HEADERS} ${d}/loaddir.h)
+
+SET(d "cplusplus/imageprovider")
+SET(photoqt_HEADERS ${photoqt_HEADERS} ${d}/imageproviderthumbnail.h ${d}/imageproviderfull.h ${d}/imageprovidericon.h)
+
+SET(d "cplusplus/shortcuts")
+SET(photoqt_HEADERS ${photoqt_HEADERS} ${d}/shortcuts.h)
+
+SET(d "cplusplus/scripts")
+SET(photoqt_HEADERS ${photoqt_HEADERS} ${d}/getanddostuff.h ${d}/getmetadata.h ${d}/runprocess.h ${d}/thumbnailsmanagement.h)
+SET(photoqt_HEADERS ${photoqt_HEADERS} ${d}/gmimagemagick.h)
+SET(d "cplusplus/scripts/getanddostuff")
+SET(photoqt_HEADERS ${photoqt_HEADERS} ${d}/openfile.h ${d}/context.h ${d}/external.h ${d}/manipulation.h ${d}/file.h ${d}/other.h)
+SET(photoqt_HEADERS ${photoqt_HEADERS} ${d}/shortcuts.h ${d}/wallpaper.h)
+
+SET(d "cplusplus/settings")
+SET(photoqt_HEADERS ${photoqt_HEADERS} ${d}/settings.h ${d}/settingssession.h ${d}/fileformats.h)
+
+SET(d "cplusplus/singleinstance")
+SET(photoqt_HEADERS ${photoqt_HEADERS} ${d}/singleinstance.h)
---
+++ CMake/ListFilesQML.cmake
@@ -0,0 +1,50 @@
+#####################
+#### QML SOURCES ####
+#####################
+
+SET(d "qml")
+SET(photoqt_SOURCES ${photoqt_SOURCES} ${d}/mainwindow.qml ${d}/Shortcuts.qml ${d}/Colour.qml)
+
+SET(d "qml/slidein")
+SET(photoqt_SOURCES ${photoqt_SOURCES} ${d}/ThumbnailBar.qml ${d}/MetaData.qml ${d}/MainMenu.qml ${d}/SlideshowBar.qml)
+
+SET(d "qml/mainview")
+SET(photoqt_SOURCES ${photoqt_SOURCES} ${d}/Background.qml ${d}/QuickInfo.qml ${d}/ContextMenu.qml ${d}/ContextMenuEntry.qml)
+SET(photoqt_SOURCES ${photoqt_SOURCES} ${d}/SmartImage.qml ${d}/MainView.qml)
+
+SET(d "qml/openfile")
+SET(photoqt_SOURCES ${photoqt_SOURCES} ${d}/OpenFile.qml ${d}/BreadCrumbs.qml ${d}/UserPlaces.qml ${d}/Folders.qml ${d}/FilesView.qml)
+SET(photoqt_SOURCES ${photoqt_SOURCES} ${d}/EditFiles.qml ${d}/TweaksZoom.qml ${d}/Tweaks.qml ${d}/TweaksViewMode.qml ${d}/TweaksPreview.qml)
+
+SET(d "qml/elements")
+SET(photoqt_SOURCES ${photoqt_SOURCES} ${d}/ScrollBarHorizontal.qml ${d}/ScrollBarVertical.qml ${d}/SettingsText.qml)
+SET(photoqt_SOURCES ${photoqt_SOURCES} ${d}/CustomCheckBox.qml ${d}/CustomButton.qml ${d}/CustomTabView.qml)
+SET(photoqt_SOURCES ${photoqt_SOURCES} ${d}/CustomComboBox.qml ${d}/CustomRadioButton.qml ${d}/CustomSlider.qml)
+SET(photoqt_SOURCES ${photoqt_SOURCES} ${d}/CustomSpinBox.qml ${d}/CustomConfirm.qml ${d}/CustomTextEdit.qml)
+SET(photoqt_SOURCES ${photoqt_SOURCES} ${d}/CustomDetectShortcut.qml ${d}/CustomMouseShortcut.qml)
+SET(photoqt_SOURCES ${photoqt_SOURCES} ${d}/CustomExternalCommand.qml ${d}/CustomLineEdit.qml ${d}/ToolTip.qml)
+
+SET(d "qml/fadein")
+SET(photoqt_SOURCES ${photoqt_SOURCES} ${d}/About.qml ${d}/Scale.qml ${d}/Rename.qml ${d}/Delete.qml)
+SET(photoqt_SOURCES ${photoqt_SOURCES} ${d}/Wallpaper.qml ${d}/Slideshow.qml ${d}/Filter.qml ${d}/Startup.qml)
+
+SET(d "qml/settings")
+SET(photoqt_SOURCES ${photoqt_SOURCES} ${d}/SettingsItem.qml ${d}/TabDetailsTile.qml ${d}/TabOtherLanguageTiles.qml)
+SET(photoqt_SOURCES ${photoqt_SOURCES} ${d}/TabLookAndFeelBasic.qml ${d}/TabLookAndFeelAdvanced.qml ${d}/TabOtherContext.qml)
+SET(photoqt_SOURCES ${photoqt_SOURCES} ${d}/TabFiletypesTiles.qml ${d}/TabShortcutsCategories.qml ${d}/TabShortcutsTilesSet.qml)
+SET(photoqt_SOURCES ${photoqt_SOURCES} ${d}/TabShortcutsTilesAvail.qml ${d}/TabThumbnailsBasic.qml ${d}/TabThumbnailsAdvanced.qml)
+SET(photoqt_SOURCES ${photoqt_SOURCES} ${d}/TabShortcuts.qml ${d}/TabDetails.qml ${d}/TabOther.qml ${d}/TabFiletypes.qml)
+
+SET(d "qml/settings2")
+SET(photoqt_SOURCES ${photoqt_SOURCES} ${d}/SettingsManager.qml ${d}/EntryTitle.qml ${d}/EntrySetting.qml ${d}/EntryContainer.qml)
+SET(d "qml/settings2/tabs")
+SET(photoqt_SOURCES ${photoqt_SOURCES} ${d}/TabLookAndFeel.qml ${d}/TabThumbnails.qml)
+SET(d "qml/settings2/tabs/lookandfeel")
+SET(photoqt_SOURCES ${photoqt_SOURCES} ${d}/SortBy.qml ${d}/WindowMode.qml ${d}/TrayIcon.qml ${d}/ClosingX.qml ${d}/FitInWindow.qml)
+SET(photoqt_SOURCES ${photoqt_SOURCES} ${d}/OverlayColor.qml ${d}/Quickinfo.qml ${d}/Background.qml ${d}/BorderAroundImage.qml)
+SET(photoqt_SOURCES ${photoqt_SOURCES} ${d}/CloseOnClick.qml ${d}/Loop.qml ${d}/Transition.qml ${d}/HotEdge.qml)
+SET(photoqt_SOURCES ${photoqt_SOURCES} ${d}/MouseWheelSensitivity.qml ${d}/Interpolation.qml ${d}/Remember.qml ${d}/Animation.qml)
+SET(d "qml/settings2/tabs/thumbnails")
+SET(photoqt_SOURCES ${photoqt_SOURCES} ${d}/ThumbnailSize.qml ${d}/Spacing.qml ${d}/LiftUp.qml ${d}/KeepVisible.qml ${d}/Dynamic.qml)
+SET(photoqt_SOURCES ${photoqt_SOURCES} ${d}/CenterOn.qml ${d}/TopOrBottom.qml ${d}/Label.qml ${d}/FilenameOnly.qml ${d}/Disable.qml)
+SET(photoqt_SOURCES ${photoqt_SOURCES} ${d}/Cache.qml)
