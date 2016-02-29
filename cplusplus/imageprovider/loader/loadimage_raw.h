a3b0674cc31e5e1988f9facfe6f7fc804ddf289b
Lukas Spies <Lukas@photoqt.org>
2016-02-29 18:08:19 -0330

    [ImageReader > RAW] Add comments, order code, add LIBRAW_IMAGE_JPEG handling, clean up memory better

--- cplusplus/imageprovider/loader/loadimage_raw.h
+++ cplusplus/imageprovider/loader/loadimage_raw.h
@@ -1,9 +1,11 @@
 #ifndef LOADIMAGE_RAW_H
 #define LOADIMAGE_RAW_H
 
+#include <QFile>
 #include <QImage>
 #include <QtDebug>
 #include <libraw/libraw.h>
+#include "errorimage.h"
 
 class LoadImageRaw {
 
@@ -11,85 +13,91 @@ public:
 
 	static QImage load(QString filename, QSize maxSize) {
 
-		bool thumb = (maxSize.width() <= 256 && maxSize.height() <= 256 && maxSize.width() > 0 && maxSize.height() > 0);
+		// Later we decide according to thumbnail/image size whether to load thumbnail or half/full image
+		bool thumb = false;
 		bool half = false;
 
+		// The LibRaw instance
 		LibRaw raw;
-		QByteArray imgData;
+		raw.recycle();
 
+		// Some settings to improve speed
+		// Since we don't care about manipulating RAW images but only want to display
+		// them, we can optimise for speed
 		raw.imgdata.params.user_qual = 1;
 		raw.imgdata.params.use_rawspeed = 1;
 
+		// Open the RAW image
 		int ret = raw.open_file((const char*)(QFile::encodeName(filename)).constData());
-
 		if(ret != LIBRAW_SUCCESS) {
-			qDebug() << "LibRaw: failed to run open_file: " << libraw_strerror(ret);
 			raw.recycle();
-			return QImage();
-		} else
-			qDebug() << "LibRaw: succesfully executed open_file";
+			return ErrorImage::load(QString("LibRaw: failed to run open_file: %1").arg(libraw_strerror(ret)));
+		}
 
-		// If an embedded thumbnail is not available -> work around by loading half-size image
-		if(thumb && raw.imgdata.thumbnail.tformat == LIBRAW_THUMBNAIL_UNKNOWN) {
-			thumb = false;
+		// Depending on the RAW image anf the requested image size, we can opt for the thumbnail or half size if that's enough
+		if(raw.imgdata.thumbnail.twidth >= maxSize.width() && raw.imgdata.thumbnail.theight >= maxSize.height()
+				&& raw.imgdata.thumbnail.tformat != LIBRAW_THUMBNAIL_UNKNOWN)
+			thumb = true;
+		else if(raw.imgdata.sizes.iwidth >= maxSize.width()*2 && raw.imgdata.sizes.iheight >= maxSize.height()) {
 			half = true;
 			raw.imgdata.params.half_size = 1;
 		}
 
-		if(!thumb && raw.imgdata.sizes.iwidth >= maxSize.width()*2 && raw.imgdata.sizes.iheight >= maxSize.height()) {
-			half = true;
-			raw.imgdata.params.half_size = 1;
-		}
-
-		if(thumb)
-			ret = raw.unpack_thumb();
-		else
-			ret = raw.unpack();
+		// Unpack the RAW image/thumbnail
+		if(thumb) ret = raw.unpack_thumb();
+		else ret = raw.unpack();
 
 		if(ret != LIBRAW_SUCCESS) {
-			qDebug() << "LibRaw: failed to run unpack: " << libraw_strerror(ret);
 			raw.recycle();
-			return QImage();
-		} else
-			qDebug() << "LibRaw: succesfully executed unpack";
+			return ErrorImage::load(QString("LibRaw: failed to run %1: %2").arg(thumb ? "unpack_thumb" : "unpack").arg(libraw_strerror(ret)));
+		}
 
-		if(!thumb)
-			ret = raw.dcraw_process();
+		// Post-process image. Not necessary for embedded preview...
+		if(!thumb) ret = raw.dcraw_process();
 
 		if (ret != LIBRAW_SUCCESS) {
-			qDebug() << "LibRaw: failed to run dcraw_process: " << libraw_strerror(ret);
 			raw.recycle();
-			return QImage();
-		} else
-			qDebug() << "LibRaw: succesfully executed dcraw_process";
-
-		libraw_processed_image_t* img;
-		if(thumb)
-			img = raw.dcraw_make_mem_thumb(&ret);
-		else
-			img = raw.dcraw_make_mem_image(&ret);
-
-
-
-		// createPPMHeader()
-		QString header = QString::fromUtf8("P%1\n%2 %3\n%4\n")
-				.arg(img->colors == 3 ? QLatin1String("6") : QLatin1String("5"))
-				.arg(img->width)
-				.arg(img->height)
-				.arg((1 << img->bits)-1);
-		imgData.append(header.toLatin1());
-		imgData.append(QByteArray((const char*)img->data, (int)img->data_size));
-
-		if(imgData.isEmpty()) {
-			qDebug() << "Failed to load " << (half ? "half preview" : (thumb ? "thumbnail" : "image")) << " from LibRaw!";
-			return QImage();
+			return ErrorImage::load(QString("LibRaw: failed to run dcraw_process: %1").arg(libraw_strerror(ret)));
 		}
+
+		// Create processed image
+		libraw_processed_image_t* img;
+		if(thumb) img = raw.dcraw_make_mem_thumb(&ret);
+		else img = raw.dcraw_make_mem_image(&ret);
+
+
+		// This will hold the loaded image data
+		QByteArray imgData;
+
 
 		QImage image;
 
-		if(!image.loadFromData(imgData)) {
-			qDebug() << "Failed to load PPM data from LibRaw!";
-			return QImage();
+		if(img->type == LIBRAW_IMAGE_JPEG) {
+
+			image.loadFromData(img->data, img->data_size, "JPEG");
+
+		} else {
+
+			// Create a header and load the image data into QByteArray
+			QString header = QString::fromUtf8("P%1\n%2 %3\n%4\n")
+					.arg(img->colors == 3 ? QLatin1String("6") : QLatin1String("5"))
+					.arg(img->width)
+					.arg(img->height)
+					.arg((1 << img->bits)-1);
+			imgData.append(header.toLatin1());
+			imgData.append(QByteArray((const char*)img->data, (int)img->data_size));
+
+			if(imgData.isEmpty()) {
+				raw.recycle();
+				return ErrorImage::load("Failed to load " + QString(half ? "half preview" : (thumb ? "thumbnail" : "image")) + " from LibRaw!");
+			}
+
+			// The return image is loaded from the QByteArray above
+			if(!image.loadFromData(imgData)) {
+				raw.recycle();
+				return ErrorImage::load("Failed to load PPM data from LibRaw!");
+			}
+
 		}
 
 		// Store origSize in file for later detection
@@ -106,6 +114,10 @@ public:
 				}
 			}
 		}
+
+		// Clean up memory
+		raw.dcraw_clear_mem(img);
+		raw.recycle();
 
 		return image;
 
