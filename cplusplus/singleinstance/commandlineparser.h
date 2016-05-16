fb8465e513d0030c90247a4470df52216d523417
Lukas Spies <Lukas@photoqt.org>
2016-05-16 16:51:17 -0230

    [Qt 5.3] Fix: QCommandLineParser::addOptions() wasn't introduced until version Qt 5.4

--- cplusplus/singleinstance/commandlineparser.h
+++ cplusplus/singleinstance/commandlineparser.h
@@ -21,26 +21,23 @@ public:
 		parser.addHelpOption();
 		parser.addVersionOption();
 
-		// Add custom PhotoQt options (C++11 way)
-		parser.addOptions({
-				{{"o","open"},
-				 tr("Make PhotoQt ask for a new File")},
-				{{"t","toggle"},
-				 tr("Toggle PhotoQt - hides PhotoQt if visible, shows if hidden")},
-				{{"s","show"},
-				 tr("Shows PhotoQt (does nothing if already shown)")},
-				{{"h","hide"},
-				 tr("Hides PhotoQt (does nothing if already hidden)")},
-				{"no-thumbs",
-				 tr("Disable thumbnails")},
-				{"thumbs",
-				 tr("Enable thumbnails")},
-				{"start-in-tray",
-				 tr("Start PhotoQt hidden to the system tray (at start-up only)")},
-				{"verbose",
-				 tr("Enable debug messages (at start-up only)")}
-
-		 });
+		// Add custom PhotoQt options (We HAVE to go the long way, as the shortcut possible isn't available in Qt 5.3)
+		QCommandLineOption opt_open(QStringList() << "o" << "open", tr("Make PhotoQt ask for a new File"));
+		parser.addOption(opt_open);
+		QCommandLineOption opt_toggle(QStringList() << "t" << "toggle", tr("Toggle PhotoQt - hides PhotoQt if visible, shows if hidden"));
+		parser.addOption(opt_toggle);
+		QCommandLineOption opt_show(QStringList() << "s" << "show", tr("Shows PhotoQt (does nothing if already shown)"));
+		parser.addOption(opt_show);
+		QCommandLineOption opt_hide(QStringList() << "h" << "hide", tr("Hides PhotoQt (does nothing if already hidden)"));
+		parser.addOption(opt_hide);
+		QCommandLineOption opt_nothumbs("no-thumbs", tr("Disable thumbnails"));
+		parser.addOption(opt_nothumbs);
+		QCommandLineOption opt_thumbs("thumbs", tr("Enable thumbnails"));
+		parser.addOption(opt_thumbs);
+		QCommandLineOption opt_startintray("start-in-tray", tr("Start PhotoQt hidden to the system tray (at start-up only)"));
+		parser.addOption(opt_startintray);
+		QCommandLineOption opt_verbose("verbose", tr("Enable debug messages (at start-up only)"));
+		parser.addOption(opt_verbose);
 
 		// Add optional argument 'filename'
 		parser.addPositionalArgument("filename",tr("File to open with PhotoQt"), "[filename]");
