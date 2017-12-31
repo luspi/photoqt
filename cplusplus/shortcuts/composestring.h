747cc55b8f543a12e7d4c8c9b5c02342f61a8afa
Lukas Spies <Lukas@photoqt.org>
2017-12-31 13:43:36 +0100

    [Shortcuts] Extend list of special keys for detection to most keys documented for Qt

--- cplusplus/shortcuts/composestring.h
+++ cplusplus/shortcuts/composestring.h
@@ -10,62 +10,210 @@ public:
 
         QString combostring = getModifiers(e);
 
-        if(e->key() == Qt::Key_Escape)
+        switch(e->key()) {
+        case Qt::Key_Escape:
             combostring += "Escape";
-        else if(e->key() == Qt::Key_Right)
+            break;
+        case Qt::Key_Right:
             combostring += "Right";
-        else if(e->key() == Qt::Key_Left)
+            break;
+        case Qt::Key_Left:
             combostring += "Left";
-        else if(e->key() == Qt::Key_Up)
+            break;
+        case Qt::Key_Up:
             combostring += "Up";
-        else if(e->key() == Qt::Key_Down)
+            break;
+        case Qt::Key_Down:
             combostring += "Down";
-        else if(e->key() == Qt::Key_Space)
+            break;
+        case Qt::Key_Space:
             combostring += "Space";
-        else if(e->key() == Qt::Key_Delete)
+            break;
+        case Qt::Key_Delete:
             combostring += "Delete";
-        else if(e->key() == Qt::Key_Home)
+            break;
+        case Qt::Key_Home:
             combostring += "Home";
-        else if(e->key() == Qt::Key_End)
+            break;
+        case Qt::Key_End:
             combostring += "End";
-        else if(e->key() == Qt::Key_PageUp)
+            break;
+        case Qt::Key_PageUp:
             combostring += "Page Up";
-        else if(e->key() == Qt::Key_PageDown)
+            break;
+        case Qt::Key_PageDown:
             combostring += "Page Down";
-        else if(e->key() == Qt::Key_Insert)
+            break;
+        case Qt::Key_Insert:
             combostring += "Insert";
-        else if(e->key() == Qt::Key_Tab || e->key() == Qt::Key_Backtab)
+            break;
+        case Qt::Key_Tab:
             combostring += "Tab";
-        else if(e->key() == Qt::Key_Return)
+            break;
+        case Qt::Key_Backtab:
+            combostring += "Tab";
+            break;
+        case Qt::Key_Return:
             combostring += "Return";
-        else if(e->key() == Qt::Key_Enter)
+            break;
+        case Qt::Key_Enter:
             combostring += "Enter";
-        else if(e->key() == Qt::Key_F1)
+            break;
+        case Qt::Key_Pause:
+            combostring += "Pause";
+            break;
+        case Qt::Key_Print:
+            combostring += "Print";
+            break;
+        case Qt::Key_SysReq:
+            combostring += "SysReq";
+            break;
+        case Qt::Key_Clear:
+            combostring += "Clear";
+            break;
+        case Qt::Key_CapsLock:
+            combostring += "CapsLock";
+            break;
+        case Qt::Key_NumLock:
+            combostring += "NumLock";
+            break;
+        case Qt::Key_ScrollLock:
+            combostring += "ScrollLock";
+            break;
+        case Qt::Key_Super_L:
+            combostring += "Super L";
+            break;
+        case Qt::Key_Super_R:
+            combostring += "Super R";
+            break;
+        case Qt::Key_Menu:
+            combostring += "Menu";
+            break;
+        case Qt::Key_Hyper_L:
+            combostring += "Hyper L";
+            break;
+        case Qt::Key_Hyper_R:
+            combostring += "Hyper R";
+            break;
+        case Qt::Key_Help:
+            combostring += "Help";
+            break;
+        case Qt::Key_Direction_L:
+            combostring += "Direction L";
+            break;
+        case Qt::Key_Direction_R:
+            combostring += "Direction R";
+            break;
+        case Qt::Key_F1:
             combostring += "F1";
-        else if(e->key() == Qt::Key_F2)
+            break;
+        case Qt::Key_F2:
             combostring += "F2";
-        else if(e->key() == Qt::Key_F3)
+            break;
+        case Qt::Key_F3:
             combostring += "F3";
-        else if(e->key() == Qt::Key_F4)
+            break;
+        case Qt::Key_F4:
             combostring += "F4";
-        else if(e->key() == Qt::Key_F5)
+            break;
+        case Qt::Key_F5:
             combostring += "F5";
-        else if(e->key() == Qt::Key_F6)
+            break;
+        case Qt::Key_F6:
             combostring += "F6";
-        else if(e->key() == Qt::Key_F7)
+            break;
+        case Qt::Key_F7:
             combostring += "F7";
-        else if(e->key() == Qt::Key_F8)
+            break;
+        case Qt::Key_F8:
             combostring += "F8";
-        else if(e->key() == Qt::Key_F9)
+            break;
+        case Qt::Key_F9:
             combostring += "F9";
-        else if(e->key() == Qt::Key_F10)
+            break;
+        case Qt::Key_F10:
             combostring += "F10";
-        else if(e->key() == Qt::Key_F11)
+            break;
+        case Qt::Key_F11:
             combostring += "F11";
-        else if(e->key() == Qt::Key_F12)
+            break;
+        case Qt::Key_F12:
             combostring += "F12";
-        else if(e->text().length() > 0)
-            combostring += QKeySequence(e->key()).toString();
+            break;
+        case Qt::Key_F13:
+            combostring += "F13";
+            break;
+        case Qt::Key_F14:
+            combostring += "F14";
+            break;
+        case Qt::Key_F15:
+            combostring += "F15";
+            break;
+        case Qt::Key_F16:
+            combostring += "F16";
+            break;
+        case Qt::Key_F17:
+            combostring += "F17";
+            break;
+        case Qt::Key_F18:
+            combostring += "F18";
+            break;
+        case Qt::Key_F19:
+            combostring += "F19";
+            break;
+        case Qt::Key_F20:
+            combostring += "F20";
+            break;
+        case Qt::Key_F21:
+            combostring += "F21";
+            break;
+        case Qt::Key_F22:
+            combostring += "F22";
+            break;
+        case Qt::Key_F23:
+            combostring += "F23";
+            break;
+        case Qt::Key_F24:
+            combostring += "F24";
+            break;
+        case Qt::Key_F25:
+            combostring += "F25";
+            break;
+        case Qt::Key_F26:
+            combostring += "F26";
+            break;
+        case Qt::Key_F27:
+            combostring += "F27";
+            break;
+        case Qt::Key_F28:
+            combostring += "F28";
+            break;
+        case Qt::Key_F29:
+            combostring += "F29";
+            break;
+        case Qt::Key_F30:
+            combostring += "F30";
+            break;
+        case Qt::Key_F31:
+            combostring += "F31";
+            break;
+        case Qt::Key_F32:
+            combostring += "F32";
+            break;
+        case Qt::Key_F33:
+            combostring += "F33";
+            break;
+        case Qt::Key_F34:
+            combostring += "F34";
+            break;
+        case Qt::Key_F35:
+            combostring += "F35";
+            break;
+        default:
+            if(e->text() != "")
+                combostring += QKeySequence(e->key()).toString();
+
+        }
 
         return combostring;
 
