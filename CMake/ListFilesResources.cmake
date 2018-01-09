056e2419c0b5b27dc414a81ff2682fe061b366f9
Lukas Spies <Lukas@photoqt.org>
2018-01-09 11:09:06 +0100

    [localisation] add language resource

--- CMake/ListFilesResources.cmake
+++ CMake/ListFilesResources.cmake
@@ -3,5 +3,8 @@ SET(d "qmlresources")
 SET(photoqt_RESOURCES ${photoqt_RESOURCES} ${d}/elements.qrc ${d}/mainview.qrc ${d}/openfile.qrc ${d}/other.qrc ${d}/shortcuts.qrc ${d}/vars.qrc)
 SET(photoqt_RESOURCES ${photoqt_RESOURCES} ${d}/settingsmanager.qrc ${d}/slideshow.qrc ${d}/filemanagement.qrc ${d}/wallpaper.qrc)
 
+# Add language resource file
+SET(photoqt_RESOURCES ${photoqt_RESOURCES} ${CMAKE_CURRENT_BINARY_DIR}/lang.qrc)
+
 # And the images
 SET(photoqt_RESOURCES ${photoqt_RESOURCES} img.qrc)
