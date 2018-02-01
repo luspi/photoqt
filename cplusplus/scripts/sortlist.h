4592a31d327c3de7452ab6817718f7c5c790ddbf
Lukas Spies <Lukas@photoqt.org>
2018-02-01 18:46:49 +0100

    [sortlist.h] fix: make file compilable down to Qt version 5.5

--- cplusplus/scripts/sortlist.h
+++ cplusplus/scripts/sortlist.h
@@ -22,16 +22,16 @@ namespace Sort {
                                             file2.fileName()) < 0;
                 });
             else
-                std::sort(list->rbegin(), list->rend(), [&collator](const QFileInfo &file1, const QFileInfo &file2) {
-                    return collator.compare(file1.fileName(),
-                                            file2.fileName()) < 0;
+                std::sort(list->begin(), list->end(), [&collator](const QFileInfo &file1, const QFileInfo &file2) {
+                    return collator.compare(file2.fileName(),
+                                            file1.fileName()) < 0;
                 });
 
         } else if(sortby == "date") {
 
             collator.setNumericMode(true);
 
-    #if (QT_VERSION >= QT_VERSION_CHECK(5, 10, 0))
+#if (QT_VERSION >= QT_VERSION_CHECK(5, 10, 0))
 
             if(sortbyAscending)
                 std::sort(list->begin(), list->end(), [&collator](const QFileInfo &file1, const QFileInfo &file2) {
@@ -44,7 +44,7 @@ namespace Sort {
                                             QString::number(file2.metadataChangeTime().toMSecsSinceEpoch())) < 0;
                 });
 
-    #else
+#else // Qt < 5.10
 
             if(sortbyAscending)
                 std::sort(list->begin(), list->end(), [&collator](const QFileInfo &file1, const QFileInfo &file2) {
@@ -52,12 +52,12 @@ namespace Sort {
                                             QString::number(file2.created().toMSecsSinceEpoch())) < 0;
                 });
             else
-                std::sort(list->rbegin(), list->rend(), [&collator](const QFileInfo &file1, const QFileInfo &file2) {
-                    return collator.compare(QString::number(file1.created().toMSecsSinceEpoch()),
-                                            QString::number(file2.created().toMSecsSinceEpoch())) < 0;
+                std::sort(list->begin(), list->end(), [&collator](const QFileInfo &file1, const QFileInfo &file2) {
+                    return collator.compare(QString::number(file2.created().toMSecsSinceEpoch()),
+                                            QString::number(file1.created().toMSecsSinceEpoch())) < 0;
                 });
 
-    #endif
+#endif // (QT_VERSION >= QT_VERSION_CHECK(5, 10, 0))
 
         } else if(sortby == "size") {
 
@@ -69,9 +69,9 @@ namespace Sort {
                                             QString::number(file2.size())) < 0;
                 });
             else
-                std::sort(list->rbegin(), list->rend(), [&collator](const QFileInfo &file1, const QFileInfo &file2) {
-                    return collator.compare(QString::number(file1.size()),
-                                            QString::number(file2.size())) < 0;
+                std::sort(list->begin(), list->end(), [&collator](const QFileInfo &file1, const QFileInfo &file2) {
+                    return collator.compare(QString::number(file2.size()),
+                                            QString::number(file1.size())) < 0;
                 });
 
         } else { // default to naturalname
@@ -84,9 +84,9 @@ namespace Sort {
                                             file2.fileName()) < 0;
                 });
             else
-                std::sort(list->rbegin(), list->rend(), [&collator](const QFileInfo &file1, const QFileInfo &file2) {
-                    return collator.compare(file1.fileName(),
-                                            file2.fileName()) < 0;
+                std::sort(list->begin(), list->end(), [&collator](const QFileInfo &file1, const QFileInfo &file2) {
+                    return collator.compare(file2.fileName(),
+                                            file1.fileName()) < 0;
                 });
 
         }
