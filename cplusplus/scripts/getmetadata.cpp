f48efbf5cb8c5d5daf155f81c078870e25543309
Lukas Spies <Lukas@photoqt.org>
2018-04-30 11:00:23 +0200

    [ManagePeopleTags / GetMetaData] on exiv2 exception, display error code AND error message

--- cplusplus/scripts/getmetadata.cpp
+++ cplusplus/scripts/getmetadata.cpp
@@ -203,7 +203,7 @@ QVariantMap GetMetaData::getExiv2(QString path) {
                 }
 
             } catch(Exiv2::Error &e) {
-                LOG << CURDATE << "GetMetaData::getExiv2() ERROR reading Exif metadata: " << e << NL;
+                LOG << CURDATE << "GetMetaData::getExiv2() Unable to read Exif metadata: " << e << " (" << Exiv2::errMsg(e.code()) << ")" << NL;
             }
 
             // If GPS is set, compose into one string
@@ -246,7 +246,7 @@ QVariantMap GetMetaData::getExiv2(QString path) {
                 }
 
             } catch(Exiv2::Error &e) {
-                LOG << CURDATE << "GetMetaData::getExiv2() ERROR reading IPTC metadata: " << e << NL;
+                LOG << CURDATE << "GetMetaData::getExiv2() ERROR reading IPTC metadata: " << e << " (" << Exiv2::errMsg(e.code()) << ")" << NL;
             }
 
             QString city = returnMap["Iptc.Application2.City"].toString();
