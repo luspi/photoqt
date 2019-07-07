71d5d35745b633509e2c410d9121ceb50ab159b9
Lukas Spies <Lukas@photoqt.org>
2019-07-07 11:28:18 -0500

    FileDialog: fix fallback icons

--- cplusplus/imageprovider/imageprovidericon.cpp
+++ cplusplus/imageprovider/imageprovidericon.cpp
@@ -42,16 +42,15 @@ QPixmap PQImageProviderIcon::requestPixmap(const QString &icon, QSize *, const Q
     // If icon is not available or if on Windows, choose from a small selection of custom provided icons
     // These backup icons are taken from the Breese-Dark icon theme, created by KDE/Plasma
     if(ret.isNull()) {
-        if(qgetenv("PHOTOQT_DEBUG") == "yes")
-            LOG << CURDATE << "ImageProviderIcon: Icon not found in theme, using fallback icon: " << icon.toStdString() << NL;
-        if(QFile(":/img/openfile/backupicons/" + icon + ".svg").exists())
-            ret = QIcon(":/img/openfile/backupicons/" + icon + ".svg");
+        LOG << CURDATE << "ImageProviderIcon: Icon not found in theme, using fallback icon: " << icon.toStdString() << NL;
+        if(QFile(":/filedialog/backupicons/" + icon + ".svg").exists())
+            ret = QIcon(":/filedialog/backupicons/" + icon + ".svg");
         else if(icon.contains("folder") || icon.contains("directory"))
-            ret = QIcon(":/img/openfile/backupicons/folder.svg");
+            ret = QIcon(":/filedialog/backupicons/folder.svg");
         else if(icon.contains("image"))
-            ret = QIcon(":/img/openfile/backupicons/image.svg");
+            ret = QIcon(":/filedialog/backupicons/image.svg");
         else
-            ret = QIcon(":/img/openfile/backupicons/unknown.svg");
+            ret = QIcon(":/filedialog/backupicons/unknown.svg");
     }
 
     return QPixmap(ret.pixmap(use));
