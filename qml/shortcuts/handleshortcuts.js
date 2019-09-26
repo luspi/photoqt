e8700d72845ee80be4b66302fa2b1ed36513db95
Lukas Spies <Lukas@photoqt.org>
2019-09-25 20:29:37 -0500

    [handleshortcuts] enable shortcut to directly permanently delete file, add shortcut to directly move file to trash

--- qml/shortcuts/handleshortcuts.js
+++ qml/shortcuts/handleshortcuts.js
@@ -1,3 +1,5 @@
+Qt.include("../loadfiles.js")
+
 function checkComboForShortcut(combo) {
 
     for(var i = 0; i < variables.shortcuts.length; ++i) {
@@ -17,26 +19,24 @@ function executeInternalFunction(func) {
 
 function whatToDoWithFoundShortcut(sh) {
 
-    console.log("SH:", sh)
-
     var cmd = sh[2]
 
-        if(cmd === "__quit")
-            toplevel.quitPhotoQt()
-        else if(cmd === "__close")
-            toplevel.closePhotoQt()
-//        else if(cmd === "__settings")
-//            call.show("settingsmanager")
-    /*else*/ if(cmd === "__next")
+    if(cmd === "__quit")
+        toplevel.quitPhotoQt()
+    else if(cmd === "__close")
+        toplevel.closePhotoQt()
+//    else if(cmd === "__settings")
+//        call.show("settingsmanager")
+    else if(cmd === "__next")
         imageitem.loadNextImage()
     else if(cmd === "__prev")
         imageitem.loadPrevImage()
-//        else if(cmd === "__about")
-//            call.show("about")
+//    else if(cmd === "__about")
+//        call.show("about")
     else if(cmd === "__slideshow")
         loader.show("slideshowsettings")
-//        else if(cmd === "__filterImages")
-//            call.show("filter")
+//    else if(cmd === "__filterImages")
+//        call.show("filter")
     else if(cmd === "__slideshowQuick") {
         loader.ensureItIsReady("slideshowcontrols")
         loader.passOn("slideshowcontrols", "start", undefined)
@@ -49,55 +49,63 @@ function whatToDoWithFoundShortcut(sh) {
     else if(cmd === "__zoomReset")
         imageitem.zoomReset()
     else if(cmd === "__zoomActual")
-            imageitem.zoomActual()
-        else if(cmd === "__rotateL")
-            imageitem.rotate(-90)
-        else if(cmd === "__rotateR")
-            imageitem.rotate(90)
-        else if(cmd === "__rotate0")
-            imageitem.rotateReset()
-        else if(cmd === "__flipH")
-            imageitem.mirrorH()
-        else if(cmd === "__flipV")
-            imageitem.mirrorV()
-        else if(cmd === "__flipReset")
-            imageitem.mirrorReset()
-        else if(cmd === "__rename")
-            loader.show("filerename")
-        else if(cmd === "__delete")
-            loader.show("filedelete")
-//        else if(cmd === "__deletePermanent")
-//            call.load("permanentDeleteFile")
-    else if(cmd === "__hideMeta")
+        imageitem.zoomActual()
+    else if(cmd === "__rotateL")
+        imageitem.rotate(-90)
+    else if(cmd === "__rotateR")
+        imageitem.rotate(90)
+    else if(cmd === "__rotate0")
+        imageitem.rotateReset()
+    else if(cmd === "__flipH")
+        imageitem.mirrorH()
+    else if(cmd === "__flipV")
+        imageitem.mirrorV()
+    else if(cmd === "__flipReset")
+        imageitem.mirrorReset()
+    else if(cmd === "__rename")
+        loader.show("filerename")
+    else if(cmd === "__delete")
+        loader.show("filedelete")
+    else if(cmd === "__deletePermanent") {
+        if(variables.indexOfCurrentImage != -1 && handlingFileManagement.deleteFile(variables.allImageFilesInOrder[variables.indexOfCurrentImage], true)) {
+            removeCurrentFilenameFromList(variables.allImageFilesInOrder[variables.indexOfCurrentImage])
+            thumbnails.reloadThumbnails()
+        }
+    } else if(cmd === "__deleteTrash") {
+        if(variables.indexOfCurrentImage != -1 && handlingFileManagement.deleteFile(variables.allImageFilesInOrder[variables.indexOfCurrentImage], false)) {
+            removeCurrentFilenameFromList(variables.allImageFilesInOrder[variables.indexOfCurrentImage])
+            thumbnails.reloadThumbnails()
+        }
+    } else if(cmd === "__hideMeta")
         loader.passOn("metadata", "toggle", undefined)
     else if(cmd === "__gotoFirstThb")
         imageitem.loadFirstImage()
     else if(cmd === "__gotoLastThb")
         imageitem.loadLastImage()
-//        else if(cmd === "__wallpaper")
-//            call.show("wallpaper")
-//        else if(cmd === "__scale")
-//            call.show("scale")
-//        else if(cmd === "__playPauseAni")
-//            imageitem.playPauseAnimation()
-//        else if(cmd === "__imgur")
-//            call.show("imgurfeedback")
-//        else if(cmd === "__imgurAnonym")
-//            call.show("imgurfeedbackanonym")
-//        else if(cmd === "__defaultFileManager")
-//            getanddostuff.openInDefaultFileManager(variables.currentDir + "/" + variables.currentFile)
-        else if(cmd === "__histogram") {
-            loader.ensureItIsReady("histogram")
-            PQSettings.histogram = !PQSettings.histogram
-        }
+//    else if(cmd === "__wallpaper")
+//        call.show("wallpaper")
+//    else if(cmd === "__scale")
+//        call.show("scale")
+//    else if(cmd === "__playPauseAni")
+//        imageitem.playPauseAnimation()
+//    else if(cmd === "__imgur")
+//        call.show("imgurfeedback")
+//    else if(cmd === "__imgurAnonym")
+//        call.show("imgurfeedbackanonym")
+//    else if(cmd === "__defaultFileManager")
+//        getanddostuff.openInDefaultFileManager(variables.currentDir + "/" + variables.currentFile)
+    else if(cmd === "__histogram") {
+        loader.ensureItIsReady("histogram")
+        PQSettings.histogram = !PQSettings.histogram
+    }
 //    else if(cmd === "__clipboard")
-//            getanddostuff.clipboardSetImage(variables.currentDir + "/" + variables.currentFile)
-//        else if(cmd === "__tagFaces")
-//            call.requestTagFaces()
-//        else {
-//            getanddostuff.executeApp(cmd, variables.currentDir + "/" + variables.currentFile)
-//            if(close !== undefined && close === true)
-//                mainwindow.closePhotoQt()
-//        }
+//        getanddostuff.clipboardSetImage(variables.currentDir + "/" + variables.currentFile)
+//    else if(cmd === "__tagFaces")
+//        call.requestTagFaces()
+//    else {
+//        getanddostuff.executeApp(cmd, variables.currentDir + "/" + variables.currentFile)
+//        if(close !== undefined && close === true)
+//            mainwindow.closePhotoQt()
+//    }
 
 }
