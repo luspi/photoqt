849fac3c29b7521daa70b1db7408beb94178bafa
Lukas Spies <Lukas@photoqt.org>
2022-03-23 00:35:00 -0500

    [PQPrintSupport] fix positioning of images to be printed

--- cplusplus/print/printsupport.cpp
+++ cplusplus/print/printsupport.cpp
@@ -27,7 +27,8 @@ void PQPrintSupport::printFile(QString filename) {
 
     PQTabImageOptions *imageoptions = new PQTabImageOptions;
 
-    QPrinter printer;
+    // show the printer dialog with additional options tab
+    QPrinter printer(QPrinter::HighResolution);
     QPrintDialog printDialog(&printer, nullptr);
     printDialog.setWindowTitle(tr("Print Photo"));
     printDialog.setOptionTabs({imageoptions});
@@ -38,39 +39,53 @@ void PQPrintSupport::printFile(QString filename) {
     if(imageprovider == nullptr)
          imageprovider = new PQImageProviderFull;
 
+    // Get the image
     QPixmap pixmap = QPixmap::fromImage(imageprovider->requestImage(filename, new QSize, QSize()));
 
+    // this is only used to figure out the proper scaling of the viewport below
+    QPrinter screenprinter(QPrinter::ScreenResolution);
+
+    // create a painter and create handles for some much used properties
     QPainter painter(&printer);
     QRect viewport = painter.viewport();
-    QSize pixsize = pixmap.size();
+    QSize pixsize = pixmap.size()*(printer.resolution()/screenprinter.resolution());
 
+    // scale image to fit to page (if requested)
     if(imageoptions->getScalingFitToPage()) {
         if(pixsize.width() > viewport.width() || pixsize.height() > viewport.height() || imageoptions->getScalingEnlargeSmaller())
             pixsize.scale(viewport.size(), Qt::KeepAspectRatio);
     }
 
-    painter.setViewport(viewport.x(), viewport.y(), pixsize.width(), pixsize.height());
-    painter.setWindow(pixmap.rect());
-
+    // get the image position the user selected in the options tab
     int imgPos = imageoptions->getImagePosition();
 
+    // the final position of the image on the page is stored in these
+    // these default values are horizontal left and vcertical top
     int x = 0;
     int y = 0;
 
+    // horizontal center
     if((imgPos+1)%3 == 0)
         x = (viewport.width()-pixsize.width())/2;
+    // horizontal right
     else if(imgPos%3 == 0)
         x = viewport.width()-pixsize.width();
 
+    // vertical center
     if(imgPos > 3 && imgPos < 7)
         y = (viewport.height()-pixsize.height())/2;
+    // vertical bottom
     else if(imgPos > 6)
         y = viewport.height()-pixsize.height();
 
-//    painter.pi
+    // set properties to painter
+    painter.setViewport(x, y, pixsize.width(), pixsize.height());
+    painter.setWindow(pixmap.rect());
 
-    painter.drawPixmap(x, y, pixmap);
+    // and draw final image
+    painter.drawPixmap(0, 0, pixmap);
 
+    // done!
     painter.end();
 
 }
