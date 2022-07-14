6247d767b0583c6591ff0f1689a883179f6b1b95
Lukas Spies <Lukas@photoqt.org>
2022-07-14 15:03:40 -0500

    Window: Use OpenGLES explicitely

--- cplusplus/main.cpp
+++ cplusplus/main.cpp
@@ -79,6 +79,9 @@ int main(int argc, char **argv) {
 
     // needs to be set before Q*Application is created
     QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+#ifdef Q_OS_WIN
+    QApplication::setAttribute(Qt::AA_UseOpenGLES);     // This appears to avoid some glitches with OpenGL and fullscreen mode on Windows
+#endif
 
     // silence the `deprecated connection' warnings
     QLoggingCategory::setFilterRules("qt.qml.connections.warning=false");
