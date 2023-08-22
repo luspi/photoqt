8ffae1e1ddd58e52c7cfdd5acbea6cfa867bcb01
Lukas Spies <Lukas@photoqt.org>
2023-08-22 17:40:11 -0500

    [PQSCriptsMetaData] only mark only function called my qml with Q_INVOKABLE

--- cplusplus/header/scripts/pqc_scriptsmetadata.h
+++ cplusplus/header/scripts/pqc_scriptsmetadata.h
@@ -17,16 +17,17 @@ public:
     PQCScriptsMetaData(PQCScriptsMetaData const&)     = delete;
     void operator=(PQCScriptsMetaData const&) = delete;
 
-    Q_INVOKABLE QString analyzeDateTimeOriginal(const QString val);
-    Q_INVOKABLE QString analyzeExposureTime(const QString val);
-    Q_INVOKABLE QString analyzeFlash(const QString val);
-    Q_INVOKABLE QString analyzeSceneCaptureType(const QString val);
-    Q_INVOKABLE QString analyzeFocalLength(const QString val);
-    Q_INVOKABLE QString analyzeFNumber(const QString val);
-    Q_INVOKABLE QString analyzeLightSource(const QString val);
-    Q_INVOKABLE QString analyzeGPS(QString latRef, QString lat, QString lonRef, QString lon);
+    QString analyzeDateTimeOriginal(const QString val);
+    QString analyzeExposureTime(const QString val);
+    QString analyzeFlash(const QString val);
+    QString analyzeSceneCaptureType(const QString val);
+    QString analyzeFocalLength(const QString val);
+    QString analyzeFNumber(const QString val);
+    QString analyzeLightSource(const QString val);
+    QString analyzeGPS(QString latRef, QString lat, QString lonRef, QString lon);
+    QPointF convertGPSToDecimal(QString gpsLatRef, QString gpsLat, QString gpsLonRef, QString gpsLon);
+
     Q_INVOKABLE QString convertGPSToDecimalForOpenStreetMap(QString gps);
-    Q_INVOKABLE QPointF convertGPSToDecimal(QString gpsLatRef, QString gpsLat, QString gpsLonRef, QString gpsLon);
 
 private:
     PQCScriptsMetaData();
