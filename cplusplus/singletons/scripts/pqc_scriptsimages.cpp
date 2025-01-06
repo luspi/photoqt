628f33d5606870e5c8262896fa34fb4bae13a98a
Lukas Spies <Lukas@photoqt.org>
2025-01-06 14:07:47 +0100

    [PQCScriptsImages] add filename to notification of failed application of color profile

--- cplusplus/singletons/scripts/pqc_scriptsimages.cpp
+++ cplusplus/singletons/scripts/pqc_scriptsimages.cpp
@@ -1811,9 +1811,10 @@ bool PQCScriptsImages::applyColorProfile(QString filename, QImage &img) {
 
         if(lcms2CountFailedApplications > 5) {
             PQCSettings::get().update("imageviewColorSpaceEnable", false);
+            Q_EMIT PQCNotify::get().showNotificationMessage(QCoreApplication::translate("imageprovider", "Application of color profile failed for:") + "<br><i>" + PQCScriptsFilesPaths::get().getFilename(filename) + "</i>");
             Q_EMIT PQCNotify::get().showNotificationMessage(QCoreApplication::translate("imageprovider", "Application of color profiles failed repeatedly. Support for color spaces will be disabled, but can be enabled again in the settings manager."));
         } else {
-            Q_EMIT PQCNotify::get().showNotificationMessage(QCoreApplication::translate("imageprovider", "Application of color profile failed."));
+            Q_EMIT PQCNotify::get().showNotificationMessage(QCoreApplication::translate("imageprovider", "Application of color profile failed for:") + "<br><i>" + PQCScriptsFilesPaths::get().getFilename(filename) + "</i>");
         }
 
     }
