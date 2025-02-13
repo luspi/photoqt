521a707b185d14cdc29ee8f865682a4546c05386
Lukas Spies <Lukas@photoqt.org>
2025-02-13 08:37:23 +0100

    [*] add/update more copyright notices

--- python/chromecast_discovery.py
+++ python/chromecast_discovery.py
@@ -1,6 +1,6 @@
  ##########################################################################
  ##                                                                      ##
- ## Copyright (C) 2011-2024 Lukas Spies                                  ##
+ ## Copyright (C) 2011-2025 Lukas Spies                                  ##
  ## Contact: https://photoqt.org                                         ##
  ##                                                                      ##
  ## This file is part of PhotoQt.                                        ##
