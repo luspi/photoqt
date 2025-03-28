d19915b011418adf877500b1f542dce39dd56b9d
Lukas Spies <Lukas@photoqt.org>
2025-03-28 10:55:09 +0100

    [windows] update windows installer scripts

--- misc/generate.py
+++ misc/generate.py
@@ -374,235 +374,6 @@ if which == 'all' or which == 'windowsrc':
 # GENERATE NSI ENTRIES
 if which == 'all' or which == 'nsi':
 
-    print("Generating new FileAssociation.nsh")
-
-    cont = """/*
-_____________________________________________________________________________
-
-                       File Association
-_____________________________________________________________________________
-
- Based on code taken from http://nsis.sourceforge.net/File_Association
-
- Usage in script:
- 1. !include "FileAssociation.nsh"
- 2. [Section|Function]
-      ${FileAssociationFunction} "Param1" "Param2" "..." $var
-    [SectionEnd|FunctionEnd]
-
- FileAssociationFunction=[RegisterExtension|UnRegisterExtension]
-
-_____________________________________________________________________________
-
- ${RegisterExtension} "[executable]" "[extension]" "[description]"
-
-"[executable]"     ; executable which opens the file format
-                   ;
-"[extension]"      ; extension, which represents the file format to open
-                   ;
-"[description]"    ; description for the extension. This will be display in Windows Explorer.
-                   ;
-
-
- ${UnRegisterExtension} "[extension]" "[description]"
-
-"[extension]"      ; extension, which represents the file format to open
-                   ;
-"[description]"    ; description for the extension. This will be display in Windows Explorer.
-                   ;
-
-_____________________________________________________________________________
-
-                         Macros
-_____________________________________________________________________________
-
- Change log window verbosity (default: 3=no script)
-
- Example:
- !include "FileAssociation.nsh"
- !insertmacro RegisterExtension
- ${FileAssociation_VERBOSE} 4   # all verbosity
- !insertmacro UnRegisterExtension
- ${FileAssociation_VERBOSE} 3   # no script
-*/
-
-
-!ifndef FileAssociation_INCLUDED
-!define FileAssociation_INCLUDED
-
-!include Util.nsh
-!include LogicLib.nsh
-
-!verbose push
-!verbose 3
-!ifndef _FileAssociation_VERBOSE
-  !define _FileAssociation_VERBOSE 3
-!endif
-!verbose ${_FileAssociation_VERBOSE}
-!define FileAssociation_VERBOSE `!insertmacro FileAssociation_VERBOSE`
-!verbose pop
-
-!macro FileAssociation_VERBOSE _VERBOSE
-  !verbose push
-  !verbose 3
-  !undef _FileAssociation_VERBOSE
-  !define _FileAssociation_VERBOSE ${_VERBOSE}
-  !verbose pop
-!macroend
-
-
-
-!macro RegisterExtensionCall _EXECUTABLE _EXTENSION _DESCRIPTION
-  !verbose push
-  !verbose ${_FileAssociation_VERBOSE}
-  Push `${_DESCRIPTION}`
-  Push `${_EXTENSION}`
-  Push `${_EXECUTABLE}`
-  ${CallArtificialFunction} RegisterExtension_
-  !verbose pop
-!macroend
-
-!macro UnRegisterExtensionCall _EXTENSION _DESCRIPTION
-  !verbose push
-  !verbose ${_FileAssociation_VERBOSE}
-  Push `${_EXTENSION}`
-  Push `${_DESCRIPTION}`
-  ${CallArtificialFunction} UnRegisterExtension_
-  !verbose pop
-!macroend
-
-
-
-!define RegisterExtension `!insertmacro RegisterExtensionCall`
-!define un.RegisterExtension `!insertmacro RegisterExtensionCall`
-
-!macro RegisterExtension
-!macroend
-
-!macro un.RegisterExtension
-!macroend
-
-!macro RegisterExtension_
-  !verbose push
-  !verbose ${_FileAssociation_VERBOSE}
-
-  Exch $R2 ;exe
-  Exch
-  Exch $R1 ;ext
-  Exch
-  Exch 2
-  Exch $R0 ;desc
-  Exch 2
-  Push $0
-  Push $1
-  Push $3
-
-"""
-
-    conn2 = sqlite3.connect('icons/iconcolors.db')
-    c2 = conn2.cursor()
-
-    c2.execute("SELECT endings FROM colors ORDER BY endings")
-    data2 = c2.fetchall()
-
-    # FileAssociation.nsh
-
-    iF = 1
-    for row in data2:
-
-        endings = row[0].split(",")
-
-        for e in endings:
-
-            if iF == 1:
-                cont += f"  ${{If}} $R1 == \".{e}\"\n"
-            else:
-                cont += f"  ${{ElseIf}} $R1 == \".{e}\"\n"
-            cont += f"    StrCpy $3 \"{iF}\"\n"
-
-            iF += 1
-
-    cont += "  ${Else}\n"
-    cont += f"    StrCpy $3 \"{iF}\"\n";
-    cont += "  ${EndIf}\n"
-
-    cont += """
-  ReadRegStr $1 HKCR $R1 ""  ; read current file association
-  StrCmp "$1" "" NoBackup  ; is it empty
-  StrCmp "$1" "$R0" NoBackup  ; is it our own
-    WriteRegStr HKCR $R1 "backup_val" "$1"  ; backup current value
-NoBackup:
-  WriteRegStr HKCR $R1 "" "$R0"  ; set our file association
-
-  ReadRegStr $0 HKCR $R0 ""
-  StrCmp $0 "" 0 Skip
-    WriteRegStr HKCR "$R0" "" "$R0"
-    WriteRegStr HKCR "$R0\\shell" "" "open"
-    WriteRegStr HKCR "$R0\\DefaultIcon" "" "$R2,$3"
-Skip:
-  WriteRegStr HKCR "$R0\\shell\\open\\command" "" '"$R2" "%1"'
-
-  Pop $3
-  Pop $1
-  Pop $0
-  Pop $R2
-  Pop $R1
-  Pop $R0
-
-  !verbose pop
-!macroend
-
-!define UnRegisterExtension `!insertmacro UnRegisterExtensionCall`
-!define un.UnRegisterExtension `!insertmacro UnRegisterExtensionCall`
-
-!macro UnRegisterExtension
-!macroend
-
-!macro un.UnRegisterExtension
-!macroend
-
-!macro UnRegisterExtension_
-  !verbose push
-  !verbose ${_FileAssociation_VERBOSE}
-
-  Exch $R1 ;desc
-  Exch
-  Exch $R0 ;ext
-  Exch
-  Push $0
-  Push $1
-
-  ReadRegStr $1 HKCR $R0 ""
-  StrCmp $1 $R1 0 NoOwn ; only do this if we own it
-  ReadRegStr $1 HKCR $R0 "backup_val"
-  StrCmp $1 "" 0 Restore ; if backup="" then delete the whole key
-  DeleteRegKey HKCR $R0
-  Goto NoOwn
-
-Restore:
-  WriteRegStr HKCR $R0 "" $1
-  DeleteRegValue HKCR $R0 "backup_val"
-  DeleteRegKey HKCR $R1 ;Delete key with association name settings
-
-NoOwn:
-
-  Pop $1
-  Pop $0
-  Pop $R1
-  Pop $R0
-
-  !verbose pop
-!macroend
-
-!endif # !FileAssociation_INCLUDED
-"""
-
-    f_new = open("output/add_to_FileAssociation.nsh", "w")
-    f_new.write(cont)
-    f_new.close()
-
-    #############################################################
-
     from datetime import datetime
 
     print("Generating new installer script...")
@@ -638,6 +409,7 @@ NoOwn:
 ; - icon.ico
 ; - license.txt
 ; - photoqt-setup.nsi (this file)
+; - the windows/filetypes folder from the source tree
 ;
 ; In addition the EnVar plugin needs to be installed for NSIS:
 ; https://nsis.sourceforge.io/EnVar_plug-in
@@ -1130,8 +902,8 @@ Function FinalStepsLeave
             if endings[0] == "zip" or endings[0] == "rar" or endings[0] == "7z":
                 continue
 
-            line = f"    ${{RegisterExtension}} \"$INSTDIR\\photoqt.exe\" \".{e}\" \"{endings[0].lower()}file\"\n"
-            un_line = f"    ${{UnRegisterExtension}} \".{e}\" \"{endings[0].lower()}file\"\n"
+            line = f"    ${{RegisterExtension}} \"$INSTDIR\\photoqt.exe\" \".{e}\" \"{e.lower()}file\"\n"
+            un_line = f"    ${{UnRegisterExtension}} \".{e}\" \"{e.lower()}file\"\n"
 
             if endings[0] in ["eps", "pdf", "ps"]:
                 pdfcont += line
@@ -1217,6 +989,7 @@ Function FinalStepsLeave
     ${EndIf}
 
     WriteRegStr HKLM "${INSTDIR_REG_KEY}" "DisplayIcon" "$INSTDIR\\icon.ico"
+    WriteRegStr HKCR "Applications\\photoqt.exe" "FriendlyAppName" "PhotoQt"
 
     System::Call 'shell32.dll::SHChangeNotify(i, i, i, i) v (0x08000000, 0, 0, 0)'
 
@@ -1266,7 +1039,7 @@ Section "Uninstall"
     !insertmacro UNINSTALL.NEW_UNINSTALL "$OUTDIR"
 
     DeleteRegKey ${INSTDIR_REG_ROOT} "${INSTDIR_REG_KEY}"
-    DeleteRegKey "Software\\PhotoQt" "Software\\PhotoQt"
+    DeleteRegKey HKCU "Software\\PhotoQt"
 
     ; Remove environment variables
     EnVar::Delete "PHOTOQT_MAGICK_CODER_MODULE_PATH"
