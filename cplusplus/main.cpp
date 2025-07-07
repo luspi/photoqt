202e0cf4d75d0f33eeebcc10eaedac9db5bd9770
Lukas Spies <Lukas@photoqt.org>
2025-07-07 15:53:03 +0200

    fix running with Qt 6.4.0

--- cplusplus/main.cpp
+++ cplusplus/main.cpp
@@ -317,6 +317,7 @@ int main(int argc, char *argv[]) {
     // In Qt 6.4 this path is not automatically added as import path meaning without this PhotoQt wont find any of its modules
     // We also cannot use loadFromModule() as that does not exist yet.
     engine.addImportPath(":/");
+
     engine.load("qrc:/PhotoQt/qml/PQMainWindowModern.qml");
 #endif
 
