d26949c5ff5b8ecc41c910384888bb078093c2ee
Lukas Spies <Lukas@photoqt.org>
2026-05-08 09:02:24 +0200

    [PQCLoadImageDevil] streamline loading and improve reliability
    Signed-off-by: Lukas Spies <Lukas@photoqt.org>

--- cplusplus/images/plugins/pqc_loadimage_devil.cpp
+++ cplusplus/images/plugins/pqc_loadimage_devil.cpp
@@ -94,11 +94,6 @@ QString PQCLoadImageDevil::load(QString filename, QSize maxSize, QSize &origSize
     // DevIL is NOT threadsafe -> need to ensure only one image is loaded at a time...
     QMutexLocker locker(&PQCLoadImageDevilMutex::get().devilMutex);
 
-    // THIS IS CURRENTLY SLIGHTLY HACKY:
-    // DevIL loads the image and then writes it to a temporary jpg file.
-    // This file is then loaded by Qt's built in image plugin and returned to the user
-    // TODO: PASSING IMAGE DIRECTLY FROM DEVIL TO QIMAGE!
-
     // Create an image id and make current
     ILuint imageID;
     ilGenImages(1, &imageID);
@@ -112,10 +107,23 @@ QString PQCLoadImageDevil::load(QString filename, QSize maxSize, QSize &origSize
 
     // load the passed on image file
 #ifdef WIN32
-    ilLoadImage(filename.toStdWString().c_str());
+    if(!ilLoadImage(filename.toStdWString().c_str())) {
 #else
-    ilLoadImage(filename.toStdString().c_str());
+    if(!ilLoadImage(filename.toStdString().c_str())) {
 #endif
+        ilDeleteImages(1, &imageID);
+        QString err = "Failed to load image with DevIL";
+        qWarning() << err;
+        return err;
+    }
+
+    // convert to a predictable format Qt understands
+    if(!ilConvertImage(IL_RGBA, IL_UNSIGNED_BYTE)) {
+        ilDeleteImages(1, &imageID);
+        QString err = "Failed to convert image with DevIL";
+        qWarning() << err;
+        return err;
+    }
 
     errormsg = checkForError();
     if(errormsg != "") {
@@ -134,84 +142,24 @@ QString PQCLoadImageDevil::load(QString filename, QSize maxSize, QSize &origSize
         return errormsg;
     }
 
-/*
-    // this would be the way to load images directly from DevIL into QImage,
-    // but DevIL seems has some issues with being used simultaneously from different threads
-    // this *will* make PhotoQt crash often (possibly not always)
-    ILubyte *bt = ilGetData();
-    if(bt == NULL) LOG << "bt is NULL!!" << NL;
-    QImage tmpimg(bt, width, height, QImage::Format_ARGB32);
-    if(tmpimg.isNull()) LOG << "QImage is NULL!!" << NL;
+    uchar* data = ilGetData();
 
-    QImage img;
+    img = QImage(data, width, height, QImage::Format_RGBA8888);
 
-    // If image needs to be scaled down, do so now
-    if(maxSize.width() > 5 && maxSize.height() > 5) {
-        double q = 1;
-        if(width > maxSize.width())
-            q = (double)maxSize.width()/(double)width;
-        if(height*q > maxSize.height())
-            q = (double)maxSize.height()/(double)height;
-        img = tmpimg.scaled(width*q, height*q);
-    } else
-        img = tmpimg.copy();
+    // DevIL owns the memory, so copy before deleting image
+    img = img.copy();
 
-    ilBindImage(0);
-    ilDeleteImages(1, &imageID);
-*/
-
-    // This is the temporary file we will load the image into
-    QString tempimage = QDir::tempPath() + "/photoqtdevil.ppm";
-
-    // Make sure DevIL can overwrite any previously created file
-    ilEnable(IL_FILE_OVERWRITE);
-
-    // Save the decoded image to this temporary file
-#ifdef WIN32
-    if(!ilSaveImage(tempimage.toStdWString().c_str())) {
-#else
-    if(!ilSaveImage(tempimage.toStdString().c_str())) {
-#endif
-        // If it fails, return error image
-        ilBindImage(0);
-        ilDeleteImages(1, &imageID);
-        errormsg = checkForError();
-        if(errormsg == "")
-            errormsg = "Failed to save image decoded with DevIL!";
+    if(img.isNull()) {
+        errormsg = "Failed to create QImage with DevIL (unknown error)!";
         qWarning() << errormsg;
         return errormsg;
     }
-
-    errormsg = checkForError();
-    if(errormsg != "") {
-        qWarning() << errormsg;
-        return errormsg;
-    }
-
-    // Create reader for temporary image
-    QImageReader reader(tempimage);
 
     bool imageIsScaled = false;
 
-    // If image needs to be scaled down, do so now
     if(maxSize.width() > 5 && maxSize.height() > 5) {
         imageIsScaled = true;
-        const QSize dispSize = QSize(width, height);
-        reader.setScaledSize(dispSize.scaled(maxSize, Qt::KeepAspectRatio));
-    }
-
-    // Clear the DevIL memory
-    ilBindImage(0);
-    ilDeleteImages(1, &imageID);
-
-    // Return read image file
-    img = reader.read();
-    QFile(tempimage).remove();
-
-    if(img.isNull() || img.size() == QSize(1,1)) {
-        errormsg = "Failed to load image with DevIL (unknown error)!";
-        qWarning() << errormsg;
-        return errormsg;
+        img = img.scaled(maxSize, Qt::KeepAspectRatio, Qt::SmoothTransformation);
     }
 
     if(!img.isNull() && PQCSettingsCPP::get().getMetadataAutoRotation()) {
