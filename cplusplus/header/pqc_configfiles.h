bed04c534255d2a2e31c0259af0bdaae03029f6c
Lukas Spies <Lukas@photoqt.org>
2026-06-18 11:14:14 +0200

    [PQCConfigFiles] improve composing strings, add imageplugins dir string
    Signed-off-by: Lukas Spies <Lukas@photoqt.org>

--- cplusplus/header/pqc_configfiles.h
+++ cplusplus/header/pqc_configfiles.h
@@ -118,13 +118,17 @@ public:
         return m_ICC_COLOR_PROFILE_DIR;
     }
 
+    const QString IMAGEPLUGINS_SETTINGS_DIR() {
+        return m_IMAGEPLUGINS_SETTINGS_DIR;
+    }
+
     void setThumbnailCacheBaseDir(QString basedir) {
 
 #ifdef PQMPORTABLETWEAKS
-        basedir = QString("%1/thumbnails/").arg(qgetenv("PHOTOQT_PORTABLE_DATA_LOCATION"));
+        basedir = qgetenv("PHOTOQT_PORTABLE_DATA_LOCATION") % "/thumbnails/";
 #else
         if(basedir.isEmpty())
-            basedir = QString("%1/thumbnails").arg(QStandardPaths::writableLocation(QStandardPaths::GenericCacheLocation));
+            basedir = QStandardPaths::writableLocation(QStandardPaths::GenericCacheLocation) % "/thumbnails/";
 #endif
 
         qWarning() << "Setting thumbnail cache base dir to:" << basedir;
@@ -133,10 +137,10 @@ public:
 
         QDir dir(basedir);
         dir.mkpath(PQCConfigFiles::get().THUMBNAIL_CACHE_DIR());
-        dir.mkpath(QString("%1/normal/").arg(PQCConfigFiles::get().THUMBNAIL_CACHE_DIR()));
-        dir.mkpath(QString("%1/large/").arg(PQCConfigFiles::get().THUMBNAIL_CACHE_DIR()));
-        dir.mkpath(QString("%1/x-large/").arg(PQCConfigFiles::get().THUMBNAIL_CACHE_DIR()));
-        dir.mkpath(QString("%1/xx-large/").arg(PQCConfigFiles::get().THUMBNAIL_CACHE_DIR()));
+        dir.mkpath(PQCConfigFiles::get().THUMBNAIL_CACHE_DIR() % "/normal/");
+        dir.mkpath(PQCConfigFiles::get().THUMBNAIL_CACHE_DIR() % "/large/");
+        dir.mkpath(PQCConfigFiles::get().THUMBNAIL_CACHE_DIR() % "/x-large/");
+        dir.mkpath(PQCConfigFiles::get().THUMBNAIL_CACHE_DIR() % "/xx-large/");
 
     }
 
@@ -146,17 +150,17 @@ private:
 
 #ifdef PQMPORTABLETWEAKS
         const QString portablefolder = qgetenv("PHOTOQT_PORTABLE_DATA_LOCATION");
-        m_CONFIG_DIR = QString("%1/config/").arg(portablefolder);
-        m_DATA_DIR = QString("%1/data/").arg(portablefolder);
-        m_CACHE_DIR = QString("%1/cache/").arg(portablefolder);
+        m_CONFIG_DIR = portablefolder % "/config/";
+        m_DATA_DIR = portablefolder % "/data/";
+        m_CACHE_DIR = portablefolder % "/cache/";
 #else
         m_CONFIG_DIR = QStandardPaths::writableLocation(QStandardPaths::AppConfigLocation);
         m_DATA_DIR = QStandardPaths::writableLocation(QStandardPaths::AppDataLocation);
         m_CACHE_DIR = QStandardPaths::writableLocation(QStandardPaths::CacheLocation);
 #endif
-        m_EXTENSION_CONFIG_DIR = QString("%1/extensions/").arg(m_CONFIG_DIR);
-        m_EXTENSION_DATA_DIR = QString("%1/extensions/").arg(m_DATA_DIR);
-        m_EXTENSION_CACHE_DIR = QString("%1/extensions/").arg(m_CACHE_DIR);
+        m_EXTENSION_CONFIG_DIR = m_CONFIG_DIR % "/extensions/";
+        m_EXTENSION_DATA_DIR = m_DATA_DIR % "/extensions/";
+        m_EXTENSION_CACHE_DIR = m_CACHE_DIR % "/extensions/";
 
         QDir dir;
         dir.mkpath(m_CONFIG_DIR);
@@ -164,41 +168,42 @@ private:
         dir.mkpath(m_CACHE_DIR);
 
 #ifdef PQMPORTABLETWEAKS
-        m_USER_TRASH_FILES = QString("%1/Trash/files").arg(portablefolder);
+        m_USER_TRASH_FILES = portablefolder % "/Trash/files";
 #else
-        m_USER_TRASH_FILES = QString("%1/Trash/files").arg(QStandardPaths::writableLocation(QStandardPaths::GenericDataLocation));
+        m_USER_TRASH_FILES = QStandardPaths::writableLocation(QStandardPaths::GenericDataLocation) % "/Trash/files";
 #endif
 
 #ifdef Q_OS_WIN
     #ifdef PQMPORTABLETWEAKS
-        m_USER_PLACES_XBEL = QString("%1/user-places-%2.xbel").arg(m_CACHE_DIR, QSysInfo::machineHostName());
+        m_USER_PLACES_XBEL = m_CACHE_DIR % "/user-places-" % QSysInfo::machineHostName() % ".xbel";
     #else
-        m_USER_PLACES_XBEL = QString("%1/user-places.xbel").arg(m_CACHE_DIR);
+        m_USER_PLACES_XBEL = m_CACHE_DIR % "/user-places.xbel";
     #endif
 #else
-        m_USER_PLACES_XBEL = QString("%1/user-places.xbel").arg(QStandardPaths::writableLocation(QStandardPaths::GenericDataLocation));
+        m_USER_PLACES_XBEL = QStandardPaths::writableLocation(QStandardPaths::GenericDataLocation) % "/user-places.xbel";
         dir.mkpath(QStandardPaths::writableLocation(QStandardPaths::GenericDataLocation));
 #endif
 
 #ifdef PQMPORTABLETWEAKS
-        m_THUMBNAIL_CACHE_DIR = QString("%1/thumbnails").arg(portablefolder);
+        m_THUMBNAIL_CACHE_DIR = portablefolder % "/thumbnails";
 #else
-        m_THUMBNAIL_CACHE_DIR = QString("%1/thumbnails").arg(QStandardPaths::writableLocation(QStandardPaths::GenericCacheLocation));
+        m_THUMBNAIL_CACHE_DIR = QStandardPaths::writableLocation(QStandardPaths::GenericCacheLocation) % "/thumbnails";
         dir.mkpath(QStandardPaths::writableLocation(QStandardPaths::GenericCacheLocation));
 #endif
 
-        m_CONTEXTMENU_DB = QString("%1/contextmenu.db").arg(CONFIG_DIR());
-        m_DEFAULTSETTINGS_DB = QString("%1/defaultsettings.db").arg(CACHE_DIR());
-        m_USERSETTINGS_DB = QString("%1/usersettings.db").arg(CONFIG_DIR());
-        m_QUICKUSERSETTINGS_FILE = QString("%1/quickusersettings").arg(CACHE_DIR());
-        m_OLDSETTINGS_DB = QString("%1/settings.db").arg(CONFIG_DIR());
-        m_SHORTCUTS_DB = QString("%1/shortcuts.db").arg(CONFIG_DIR());
-        m_LOCATION_DB = QString("%1/location.db").arg(CONFIG_DIR());
+        m_CONTEXTMENU_DB = m_CONFIG_DIR % "/contextmenu.db";
+        m_DEFAULTSETTINGS_DB = m_CACHE_DIR % "/defaultsettings.db";
+        m_USERSETTINGS_DB = m_CONFIG_DIR % "/usersettings.db";
+        m_QUICKUSERSETTINGS_FILE = m_CACHE_DIR % "/quickusersettings";
+        m_OLDSETTINGS_DB = m_CONFIG_DIR % "/settings.db";
+        m_SHORTCUTS_DB = m_CONFIG_DIR % "/shortcuts.db";
+        m_LOCATION_DB = m_CONFIG_DIR % "/location.db";
 
-        m_WINDOW_GEOMETRY_FILE = QString("%1/geometry").arg(CONFIG_DIR());
-        m_FILEDIALOG_LAST_LOCATION = QString("%1/filedialoglastlocation").arg(CACHE_DIR());
-        m_LASTOPENEDIMAGE_FILE = QString("%1/lastimageloaded").arg(CONFIG_DIR());
-        m_ICC_COLOR_PROFILE_DIR = QString("%1/icc").arg(CACHE_DIR());
+        m_WINDOW_GEOMETRY_FILE = m_CONFIG_DIR % "/geometry";
+        m_FILEDIALOG_LAST_LOCATION = m_CACHE_DIR % "/filedialoglastlocation";
+        m_LASTOPENEDIMAGE_FILE = m_CONFIG_DIR % "/lastimageloaded";
+        m_ICC_COLOR_PROFILE_DIR = m_CACHE_DIR % "/icc";
+        m_IMAGEPLUGINS_SETTINGS_DIR = m_CONFIG_DIR % "/imageplugins";
 
     }
 
@@ -222,5 +227,6 @@ private:
     QString m_FILEDIALOG_LAST_LOCATION;
     QString m_LASTOPENEDIMAGE_FILE;
     QString m_ICC_COLOR_PROFILE_DIR;
+    QString m_IMAGEPLUGINS_SETTINGS_DIR;
 
 };
